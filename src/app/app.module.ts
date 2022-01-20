a618fc8922609c290a783faa2d70794cc49e8a40
otaviotonello <otaviotonello0102@gmail.com>
2022-01-20 12:43:35 -0300

    @otaviotonello [Task: Criando página home e sobre nós]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    HomeComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
