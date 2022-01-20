f09a315974994a5c538ac2a1d22e133f87526022
unknown <borellipedroluiz@gmail.com>
2022-01-20 12:55:54 -0300

    @pedro-borelli [task: adicionando contato]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
     MenuComponent,
     RodapeComponent,
     HomeComponent,
-    SobreNosComponent
+    SobreNosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
